c23812060569a1b50905e70bb70f6bd872af1edb
Ilya Zherebtsov <izherebcov@gmail.com>
2022-05-16 14:17:13 +0400

    добавлен congig.php чтобы скрыть пароли

--- bd.php
+++ bd.php
@@ -1,13 +1,7 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$db = 'bdtask';
+require 'config.php';
+$servername = "localhost"; 
 
 $conn = mysqli_connect($servername, $username, $password, $db);
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
 
